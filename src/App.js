d1bb2a83fba9293f34dafd52cee12b1acd6eef81
anniebombanie <anniebombanie@gmail.com>
2019-07-06 23:03:25 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import Foundation from 'react-foundation';
+import SearchForm from 'components/SearchForm.js';
+import FilterCategories from 'components/FilterCategories.js';
+import DisplayResults from 'components/DisplayResults.js';
+import Footer from 'components/Footer.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      country: "",
+      city: "",
+      allEvents: [],
+      eventCategories: "",
+      userCategory: "",
+      isLoading: true,
+      displayResults: false,
+      categoryDropdown: "",
+      error: false,
+    }
+  }
+
+// user types in their country and city in form field
+// if not valid type (letters only, no num), return error
+// clear error once valid
+// if tries to press submit with incorrect input, show error
+// if correct, make API call and return categories of events listed in the search param
+// user chooses category from dropdown and hits submits
+// makes second api call and list all events matching
+
+
 
   render() {
     return (
       <div className="App">
-        <p>working</p>
+        <SearchForm />
+        <FilterCategories />
+        <DisplayResults />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Foundation from 'react-foundation';
+
+const Footer = () => {
+  return(
+    <footer>
+      <p>Created by <a href="http://www.anniebombanie.com" target="_blank" aria-label="go to Annie Liew's website" rel="noopener noreferrer">Annie</a> | Using the  <a href="https://developer.ticketmaster.com/products-and-docs/apis/getting-started/" target="_blank" aria-label="go to Ticketmaster's API website" rel="noopener noreferrer">Meetup.com API</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
