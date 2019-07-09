817efcc25a0dff5ba52428c6c277c101047d8514
anniebombanie <anniebombanie@gmail.com>
2019-07-08 22:29:09 -0400

    fixed venue bug

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,9 +3,14 @@ import React from 'react';
 let ResultCard = props => {
   return (
     <div className="resultCard">
-      <p>Event Card</p>
-      <h3>{props.event.group.name}</h3>
-      <p>{props.event.id}</p>
+      <h3>Event name {props.event.name}</h3>
+      <p>Id {props.event.id}</p>
+      <p>Date {props.event.local_date}</p>
+      <p>Time {props.event.local_time}</p>
+      {props.event.venue && <p>Venue {props.event.venue.name}</p>}
+      {props.event.venue && <p>City {props.event.venue.city}</p>}
+      <p>More Info {props.event.link}</p>
+      <p>urlname {props.event.group.urlname}</p>
     </div>
   );
 };
